d66b45bce1656d39375e0c5852926e2b5809da6a
dalong <notmikeb@gmail.com>
2016-09-23 00:15:15 +0800

    add time

--- mikechenbot/echo_bot.py
+++ mikechenbot/echo_bot.py
@@ -5,19 +5,19 @@ import telebot
 
 bot = telebot.TeleBot("241532263:AAG6OidLQs6sCUPT8et_O5SZXKHz0-Du3K8")
 
-def check_str(*args):
-  def mycheck_str(message):
-    for i in args:
-      if message.text == i:
-         return True
-    return False 
-  return mycheck_str
+def genmycheckstr(*args):
+  def mycheckstr(message):
+   for i in args:
+    if message.text == i:
+       return True
+   return False
+  return mycheckstr
 
 @bot.message_handler(commands = ['start', 'help'])
 def send_welcome(message):
   bot.reply_to(message, "hello, have a nice day!")
 
-@bot.message_handler(check_str('t', 'time'))
+@bot.message_handler(func=genmycheckstr('t', 'T'))
 def show_time(message):
   d = datetime.datetime.now()
   bot.reply_to(message, repr(d))
