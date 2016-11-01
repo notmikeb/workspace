356afbe8805ce9e7c5ad1c1df37e906867904760
notmikeb <notmikeb@gmail.com>
2016-11-01 23:16:35 +0800

    qtime + digital label

--- qt/qt_tutorial/pyqt25.py
+++ qt/qt_tutorial/pyqt25.py
@@ -10,17 +10,28 @@ class mywin(QtGui.QWidget):
         timer.connect(timer, QtCore.SIGNAL("timeout()"), self.myfunction)
         self.value = 1
         timer.start(1000)
+        layout = QtGui.QBoxLayout(1)
+        self.label = QtGui.QLabel()
+        layout.addWidget(self.label)
+        self.setLayout (layout)
         
     def myfunction(self):
         print("trigger {}".format(self.value))
         self.setWindowTitle("trigger {}".format(self.value))
         self.value += 1
-        datetime = QtCore.QDateTime()
+        time = QtCore.QTime.currentTime()
+
+        if time.second() %2 == 0:
+            time_text = time.toString("hh : mm : ss")
+        else:
+            time_text = time.toString("hh   mm   ss")
+        self.label.setText(time_text)
+
         
         
         
 if __name__ == "__main__":
-    app = QtGui.QApplication()
+    app = QtGui.QApplication(sys.argv)
     m1 = mywin()
     m1.show()
     sys.exit(app.exec_())
