4f7803b0c16083534603b79a2825b4b2df2c9811
dalongtw chen <dalongtw chen>
2017-03-21 01:10:27 +0800

    add more files for testing

---
+++ sl4a/rpcclient/readme.txt
@@ -0,0 +1,5 @@
+### source code
+https://github.com/kuri65536/python-for-android
+
+### download library and utorial
+https://www.tutorialspoint.com/sl4a/sl4a_python_library.htm
--- sl4a/rpcclient/test.bat
+++ sl4a/rpcclient/test.bat
@@ -1,1 +1,6 @@
-python -c "import android; a = android.Android(('localhost', 54321));a.echo(1,2,3, 'test')" 
+echo "start service: menu interpreter : menu view : menu start service"
+echo "please replace 9999 below with sl4a's service port"
+adb forward tcp:54311 tcp:48046
+python -c "import android; a = android.Android(('localhost', 54321));a.makeToast('hello,world')" 
+
+echo "example http://stackoverflow.com/questions/11770594/using-sl4a-python-and-bluetooth"
