920a8a1ff1e41cc47bff2609fff52da7ba48c55d
notmikeb-vs2012 <notmikeb@gmail.com>
2017-04-11 21:50:46 +0800

    fix not stable

--- sl4a/rpcclient/getdev.py
+++ sl4a/rpcclient/getdev.py
@@ -1,4 +1,5 @@
 import subprocess
+import time
 
 o = subprocess.check_output("adb devices", shell = True)
 print (o)
@@ -16,35 +17,41 @@ def getPort(devs, index):
     print cmd
     o = subprocess.check_output(cmd, shell=True)
     o = o.split('\n')
-    lines = [ l for l in o if l.find('LISTEN') >= 0]  # remove all Not listen port
-    lines = [ i.replace('\r', '') for i in o if i.find('localhost') > 0 or i.find('127.0.0.1') > 0]
+    lines = o
+    lines = [ l for l in lines if l.find('LISTEN') >= 0 ]
+    lines = [ l for l in lines if l.find('localhost') >= 0]  # remove all Not listen port
+    lines = [ i.replace('\r', '') for i in lines ]
+    print(repr(lines))
     line = lines[0]
     line = [ l.replace(' ', '') for l in line.split(':') if len(l.replace(' ','')) >0 ]
-    if len(line) > 0:
+    if len(line) > 1:
         print( line[1] )
         return line[1]
     return -1
- 
-ports = []
-for i in range(len(devs)):
-   p = getPort(devs,i)
-   if p != -1:
-       ports.append( (devs[i], p) )
 
-print ("ports {}".format(ports))
+def forwardPort(devs):
+	ports = []
+	for i in range(len(devs)):
+	   p = getPort(devs,i)
+	   if p != -1:
+		   ports.append( (devs[i], p) )
 
-# dev likes 'FOTGLMD5B1703350', p likes 48545
-for index, pair in zip(range(len(ports)), ports):
-   dev, p = pair
-   print (index, dev, p)
-   cmd = "adb -s {} forward tcp:5432{} tcp:{}".format(dev, index, p)
-   print "cmd is {}".format(cmd)
-   o = subprocess.check_output(cmd, shell = True)
-   print o
+	print ("ports {}".format(ports))
 
+	# dev likes 'FOTGLMD5B1703350', p likes 48545
+	for index, pair in zip(range(len(ports)), ports):
+	   dev, p = pair
+	   print (index, dev, p)
+	   cmd = "adb -s {} forward tcp:5432{} tcp:{}".format(dev, index, p)
+	   print "cmd is {}".format(cmd)
+	   o = subprocess.check_output(cmd, shell = True)
+	   time.sleep(1)
+	   print o
 
+   
 if __name__ == "__main__":
    cmd = '''python -c "import android; a = android.Android(('localhost', 54320));a.makeToast('hello,world')"'''
+   forwardPort(devs)
    o = subprocess.check_output(cmd, shell = True)
 
    
