01dfdf953de9e9759c490a9e1397e1211cc37320
notmikeb-vs2012 <notmikeb@gmail.com>
2017-04-28 23:28:55 +0800

    fix database example

--- qt/qt1/personbrowser.py
+++ qt/qt1/personbrowser.py
@@ -1,10 +1,18 @@
 from PyQt4 import QtCore, QtGui, uic, QtSql
 import sys
 import sqlite3
+import logging
 
 # url https://www.youtube.com/watch?v=ZylRxvudgtk&t=1872s
 
 mwin, bwin = uic.loadUiType("personbrowser_ui.ui")
+
+sh  = logging.StreamHandler(sys.stdout)
+sh.setLevel(logging.DEBUG)
+logging.getLogger().setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+sh.setFormatter(formatter)
+logging.getLogger().addHandler(sh)
 
 class mywin(mwin, bwin):
     def __init__(self):
@@ -19,23 +27,48 @@ class mywin(mwin, bwin):
         self.connect(self.btn_next, QtCore.SIGNAL("clicked()"), self.on_pressed_next)
 
         self.db = QtSql.QSqlDatabase.addDatabase("QSQLITE")
-        self.db.setDatabaseName("C:\Users\dalone\PycharmProjects\workspace\qt\qt1\persons.db")
-        self.db.open()
+        self.db.setDatabaseName("persons.db")
+        a = self.db.open()
+        logging.info("open db result a %s" % (a))
         self.query = QtSql.QSqlQuery(self.db)
         self.query.exec_("SELECT id, name, email, birthdate from persons")
 
         if self.query.isActive():
             self.query.first()
-            self.on_update_edit()
+            self.on_update_edit()    
 
     def on_update_edit(self):
-        print(self.query.value(0).toString())
+        print("query.value(0) %s" % self.query.value(0).toString())
         self.edit_name.setText(self.query.value(1).toString())
         self.edit_email.setText(self.query.value(2).toString())
         self.date_birthdate.setDate( self.query.value(3).toDate())
         pass
     def on_pressed_new(self):
+        import datetime
+        updateQuery = QtSql.QSqlQuery(self.db)
+        updateQuery.prepare("insert into persons  (name, email, birthdate) values (:name, :email, :birthdate) ")
+        updateQuery.bindValue(":name", "NewOne" + datetime.datetime.now().strftime("%H%M%S"))
+        updateQuery.bindValue(":email", "")
+        updateQuery.bindValue(":birthdate", "")
+        if not updateQuery.exec_():
+            self.statusbar.showMessage(QtCore.QString() + updateQuery.lastError().text())
+        print("error text '%s'" % updateQuery.lastError().text())
+        
+        try:
+            logging.info ("lastInsertId {}".format(updateQuery.lastInsertId().toInt()))
+            id_old, r = updateQuery.lastInsertId().toInt()
+            logging.info("id {} r {}".format(id_old, r))
+        except:
+            id_old = 0
+            
+        self.query.exec_("SELECT id, name, email, birthdate from persons")    
+        self.query.first()
+        while id_old != self.query.value(0):
+            if not self.query.next():
+                break
+        self.on_update_edit()            
         pass
+        
     def on_pressed_cancel(self):
         self.btn_new.show()
 
