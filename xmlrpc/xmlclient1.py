d0252cf3f345d706cc35c25ce50a07e98fad6d3d
notmikeb <notmikeb@gmail.com>
2017-07-19 22:21:50 +0800

    move file

--- xmlrpc/xmlclient1.py
+++
@@ -1,8 +0,0 @@
-import xmlrpclib
-server = xmlrpclib.ServerProxy('http://localhost:8888')
-s = server.get_keyword_names()
-print (s)
-for i in s:
-   print ("{} args:{}".format(i,server.get_keyword_arguments(i)))
-   print ("{} docs:{}".format(i,server.get_keyword_documentation(i)))
-   #run_keyword
